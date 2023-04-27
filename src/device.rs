1997d4eb9569351194a6b1d9c0be3670eea46f5a
Fabian Torres <36959980+ftorres16@users.noreply.github.com>
2023-04-27 17:59:58 -0400

    refactor stamps to its own file

---
+++ src/device.rs
@@ -0,0 +1,75 @@
+use std::collections::BTreeSet;
+
+#[derive(Debug)]
+pub enum DType {
+    Vdd,
+    Idd,
+    Res,
+}
+
+#[derive(Debug)]
+pub struct SpiceElem {
+    pub dtype: DType,
+    pub name: String,
+    pub nodes: Vec<String>,
+    pub value: f64,
+}
+
+pub trait Stamp {
+    fn linear_stamp(&self, nodes: &BTreeSet<String>, a: &mut Vec<Vec<f64>>, b: &mut Vec<f64>);
+}
+
+impl Stamp for SpiceElem {
+    fn linear_stamp(&self, nodes: &BTreeSet<String>, a: &mut Vec<Vec<f64>>, b: &mut Vec<f64>) {
+        match self.dtype {
+            DType::Vdd => {
+                let vneg_idx = nodes.iter().position(|x| x.to_string() == self.nodes[0]);
+                let vpos_idx = nodes.iter().position(|x| x.to_string() == self.nodes[1]);
+                let is_idx = nodes
+                    .iter()
+                    .position(|x| x.to_string() == self.name)
+                    .unwrap();
+
+                b[is_idx] += self.value;
+
+                if let Some(i) = vpos_idx {
+                    a[is_idx][i] += 1.0;
+                    a[i][is_idx] += 1.0;
+                }
+
+                if let Some(i) = vneg_idx {
+                    a[is_idx][i] -= 1.0;
+                    a[i][is_idx] -= 1.0;
+                }
+            }
+            DType::Idd => {
+                let vneg_idx = nodes.iter().position(|x| x.to_string() == self.nodes[0]);
+                let vpos_idx = nodes.iter().position(|x| x.to_string() == self.nodes[1]);
+
+                if let Some(i) = vpos_idx {
+                    b[i] += self.value;
+                }
+                if let Some(i) = vneg_idx {
+                    b[i] -= self.value;
+                }
+            }
+            DType::Res => {
+                let g = 1.0 / self.value;
+
+                let vneg_idx = nodes.iter().position(|x| x.to_string() == self.nodes[0]);
+                let vpos_idx = nodes.iter().position(|x| x.to_string() == self.nodes[1]);
+
+                if let Some(i) = vneg_idx {
+                    a[i][i] += g;
+                }
+                if let Some(i) = vpos_idx {
+                    a[i][i] += g;
+                }
+                if let (Some(i), Some(j)) = (vpos_idx, vneg_idx) {
+                    a[i][j] -= g;
+                    a[j][i] -= g;
+                }
+            }
+        };
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,28 @@
 use std::collections::BTreeSet;
 
+mod device;
 mod gauss_lu;
+use crate::device::Stamp;
 
 const GND: &str = "0";
 
-#[derive(Debug)]
-enum DeviceType {
-    Vdd,
-    Idd,
-    Res,
-}
-
-#[derive(Debug)]
-struct SpiceElem {
-    dtype: DeviceType,
-    name: String,
-    nodes: Vec<String>,
-    value: f64,
-}
-
 fn main() {
-    let mut elems: Vec<SpiceElem> = Vec::new();
+    let mut elems: Vec<device::SpiceElem> = Vec::new();
 
-    elems.push(SpiceElem {
-        dtype: DeviceType::Vdd,
+    elems.push(device::SpiceElem {
+        dtype: device::DType::Vdd,
         name: "V0".to_string(),
         nodes: vec!["0".to_string(), "1".to_string()],
         value: 3.0,
     });
-    elems.push(SpiceElem {
-        dtype: DeviceType::Res,
+    elems.push(device::SpiceElem {
+        dtype: device::DType::Res,
         name: "R1".to_string(),
         nodes: vec!["1".to_string(), "2".to_string()],
         value: 1e3,
     });
-    elems.push(SpiceElem {
-        dtype: DeviceType::Res,
+    elems.push(device::SpiceElem {
+        dtype: device::DType::Res,
         name: "R1".to_string(),
         nodes: vec!["2".to_string(), "0".to_string()],
         value: 1e3,
@@ -47,7 +34,9 @@ fn main() {
     let mut b_vec = vec![0.0; nodes.len()];
     let mut x_vec = vec![0.0; nodes.len()];
 
-    load_stamps(&elems, &nodes, &mut a_mat, &mut b_vec);
+    for elem in elems.iter() {
+        elem.linear_stamp(&nodes, &mut a_mat, &mut b_vec);
+    }
 
     gauss_lu::solve(&mut a_mat, &mut b_vec, &mut x_vec);
 
@@ -56,7 +45,7 @@ fn main() {
     }
 }
 
-fn find_nodes(elems: &Vec<SpiceElem>) -> BTreeSet<String> {
+fn find_nodes(elems: &Vec<device::SpiceElem>) -> BTreeSet<String> {
     let mut nodes: BTreeSet<String> = BTreeSet::new();
 
     for elem in elems.iter() {
@@ -64,7 +53,7 @@ fn find_nodes(elems: &Vec<SpiceElem>) -> BTreeSet<String> {
             nodes.insert(node.to_string());
         }
 
-        if let DeviceType::Vdd = elem.dtype {
+        if let device::DType::Vdd = elem.dtype {
             nodes.insert(elem.name.to_string());
         }
     }
@@ -75,64 +64,4 @@ fn find_nodes(elems: &Vec<SpiceElem>) -> BTreeSet<String> {
     nodes.remove(GND);
 
     nodes
-}
-
-fn load_stamps(
-    elems: &Vec<SpiceElem>,
-    nodes: &BTreeSet<String>,
-    a_mat: &mut Vec<Vec<f64>>,
-    b_vec: &mut Vec<f64>,
-) -> () {
-    for elem in elems.iter() {
-        match elem.dtype {
-            DeviceType::Vdd => {
-                let vneg_idx = nodes.iter().position(|x| x.to_string() == elem.nodes[0]);
-                let vpos_idx = nodes.iter().position(|x| x.to_string() == elem.nodes[1]);
-                let is_idx = nodes
-                    .iter()
-                    .position(|x| x.to_string() == elem.name)
-                    .unwrap();
-
-                b_vec[is_idx] += elem.value;
-
-                if let Some(i) = vpos_idx {
-                    a_mat[is_idx][i] += 1.0;
-                    a_mat[i][is_idx] += 1.0;
-                }
-
-                if let Some(i) = vneg_idx {
-                    a_mat[is_idx][i] -= 1.0;
-                    a_mat[i][is_idx] -= 1.0;
-                }
-            }
-            DeviceType::Idd => {
-                let vneg_idx = nodes.iter().position(|x| x.to_string() == elem.nodes[0]);
-                let vpos_idx = nodes.iter().position(|x| x.to_string() == elem.nodes[1]);
-
-                if let Some(i) = vpos_idx {
-                    b_vec[i] += elem.value;
-                }
-                if let Some(i) = vneg_idx {
-                    b_vec[i] -= elem.value;
-                }
-            }
-            DeviceType::Res => {
-                let g = 1.0 / elem.value;
-
-                let vneg_idx = nodes.iter().position(|x| x.to_string() == elem.nodes[0]);
-                let vpos_idx = nodes.iter().position(|x| x.to_string() == elem.nodes[1]);
-
-                if let Some(i) = vneg_idx {
-                    a_mat[i][i] += g;
-                }
-                if let Some(i) = vpos_idx {
-                    a_mat[i][i] += g;
-                }
-                if let (Some(i), Some(j)) = (vpos_idx, vneg_idx) {
-                    a_mat[i][j] -= g;
-                    a_mat[j][i] -= g;
-                }
-            }
-        }
-    }
 }
