f0024a203c380ce90bfdb25778fccdd0e77f303f
Fabian Torres <fabian.torres.alvare+github@gmail.com>
2023-05-02 18:34:37 -0400

    parsing R and V circuits

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
 use std::collections::BTreeSet;
+use std::fs;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "spice.pest"]
+pub struct SpiceParser;
 
 mod device;
 mod gauss_lu;
+// mod scanner;
 
 const GND: &str = "0";
 
 fn main() {
-    let mut elems: Vec<device::SpiceElem> = Vec::new();
+    let unparsed_file = fs::read_to_string("test/test.sp").expect("Cannot read file.");
 
-    elems.push(device::SpiceElem {
-        dtype: device::DType::Vdd,
-        name: "V0".to_string(),
-        nodes: vec!["0".to_string(), "1".to_string()],
-        value: 3.0,
-    });
-    elems.push(device::SpiceElem {
-        dtype: device::DType::Res,
-        name: "R1".to_string(),
-        nodes: vec!["1".to_string(), "2".to_string()],
-        value: 1e3,
-    });
-    elems.push(device::SpiceElem {
-        dtype: device::DType::Res,
-        name: "R1".to_string(),
-        nodes: vec!["2".to_string(), "0".to_string()],
-        value: 1e3,
-    });
+    let file = SpiceParser::parse(Rule::file, &unparsed_file);
 
-    let nodes = find_nodes(&elems);
+    println!("{:?}", file);
 
-    let mut a_mat = vec![vec![0.0; nodes.len()]; nodes.len()];
-    let mut b_vec = vec![0.0; nodes.len()];
-    let mut x_vec = vec![0.0; nodes.len()];
-
-    for elem in elems.iter() {
-        elem.linear_stamp(&nodes, &mut a_mat, &mut b_vec);
-    }
-
-    gauss_lu::solve(&mut a_mat, &mut b_vec, &mut x_vec);
-
-    for (node, val) in nodes.iter().zip(x_vec.iter()) {
-        println!("{node}: {val}");
-    }
+    //     let mut elems: Vec<device::SpiceElem> = Vec::new();
+    //
+    //     elems.push(device::SpiceElem {
+    //         dtype: device::DType::Vdd,
+    //         name: "V0".to_string(),
+    //         nodes: vec!["0".to_string(), "1".to_string()],
+    //         value: 3.0,
+    //     });
+    //     elems.push(device::SpiceElem {
+    //         dtype: device::DType::Res,
+    //         name: "R1".to_string(),
+    //         nodes: vec!["1".to_string(), "2".to_string()],
+    //         value: 1e3,
+    //     });
+    //     elems.push(device::SpiceElem {
+    //         dtype: device::DType::Res,
+    //         name: "R1".to_string(),
+    //         nodes: vec!["2".to_string(), "0".to_string()],
+    //         value: 1e3,
+    //     });
+    //
+    //     let nodes = find_nodes(&elems);
+    //
+    //     let mut a_mat = vec![vec![0.0; nodes.len()]; nodes.len()];
+    //     let mut b_vec = vec![0.0; nodes.len()];
+    //     let mut x_vec = vec![0.0; nodes.len()];
+    //
+    //     for elem in elems.iter() {
+    //         elem.linear_stamp(&nodes, &mut a_mat, &mut b_vec);
+    //     }
+    //
+    //     gauss_lu::solve(&mut a_mat, &mut b_vec, &mut x_vec);
+    //
+    //     for (node, val) in nodes.iter().zip(x_vec.iter()) {
+    //         println!("{node}: {val}");
+    //     }
 }
 
 fn find_nodes(elems: &Vec<device::SpiceElem>) -> BTreeSet<String> {
