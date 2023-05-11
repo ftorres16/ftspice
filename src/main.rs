0c1dddc5262f1eea39d615c07651aacd06ebd00e
Fabian Torres <fabian.torres.alvare+github@gmail.com>
2023-05-11 01:01:36 -0400

    clean up main print fns

--- src/main.rs
+++ src/main.rs
@@ -22,64 +22,55 @@ fn main() {
     let mut engine = engine::Engine::new(elems, cmds);
 
     if let Some(_) = engine.op_cmd {
-        let (n_iters, x) = engine.run_op();
+        let (n_iters, mut x) = engine.run_op();
 
-        print_headers(&engine.nodes);
-        print_results(&n_iters, &x)
+        let headers = engine.nodes.keys().map(|s| s.as_str()).collect::<Vec<_>>();
+        print_headers(&headers);
+
+        x.insert(0, n_iters.clone() as f64);
+        print_results(&x);
     }
 
     if let Some(_) = engine.dc_cmd {
         let (n_iters_hist, x_hist) = engine.run_dc();
 
-        print_headers(&engine.nodes);
+        let mut headers = engine.nodes.keys().map(|s| s.as_str()).collect::<Vec<_>>();
+        headers.insert(0, "n_iters");
+        print_headers(&headers);
         for (n_iters, x) in n_iters_hist.iter().zip(x_hist) {
-            print_results(n_iters, &x);
+            let mut x = x.clone();
+            x.insert(0, n_iters.clone() as f64);
+            print_results(&x);
         }
     }
 
     if let Some(_) = engine.tran_cmd {
         let (n_iters_hist, t_hist, x_hist) = engine.run_tran();
 
-        print_headers_tran(&engine.nodes);
+        let mut headers = engine.nodes.keys().map(|s| s.as_str()).collect::<Vec<_>>();
+        headers.insert(0, "n_iters");
+        headers.insert(1, "t");
+        print_headers(&headers);
+
         for ((n_iters, t), x) in n_iters_hist.iter().zip(t_hist.iter()).zip(x_hist) {
-            print_results_tran(n_iters, t, &x);
+            let mut x = x.clone();
+            x.insert(0, n_iters.clone() as f64);
+            x.insert(1, t.clone());
+            print_results(&x);
         }
     }
 }
 
-fn print_headers(nodes: &node::NodeCollection) {
-    println!(
-        "n_iters{}",
-        nodes.keys().fold(String::new(), |a, b| a + "," + b)
-    );
+fn print_headers(row: &Vec<&str>) {
+    println!("{}", row.join(","));
 }
 
-fn print_results(n_iters: &u64, x_vec: &Vec<f64>) {
+fn print_results(row: &Vec<f64>) {
     println!(
-        "{}{}",
-        n_iters,
-        x_vec
-            .iter()
+        "{}",
+        row.iter()
             .map(|x| x.to_string())
-            .fold(String::new(), |a, b| a + "," + &b)
-    );
-}
-
-fn print_headers_tran(nodes: &node::NodeCollection) {
-    println!(
-        "n_iters,t{}",
-        nodes.keys().fold(String::new(), |a, b| a + "," + b)
-    );
-}
-
-fn print_results_tran(n_iters: &u64, t: &f64, x_vec: &Vec<f64>) {
-    println!(
-        "{},{}{}",
-        n_iters,
-        t,
-        x_vec
-            .iter()
-            .map(|x| x.to_string())
-            .fold(String::new(), |a, b| a + "," + &b)
+            .collect::<Vec<_>>()
+            .join(",")
     );
 }
